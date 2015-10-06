1dbf4c674796a5849b0efdb6d7fe9a04e8c02472
camachon18 <camachon18@up.edu>
2015-10-05 17:56:22 -0700

    Finished commenting

--- app/src/main/java/edu/up/cs371/schmidtj/football/Player.java
+++ app/src/main/java/edu/up/cs371/schmidtj/football/Player.java
@@ -1,3 +1,10 @@
+/*
+* Player.java description:
+*  Class used for Player objects in the Fantasy Football app. Constructor creates a Player object and
+*  initializes all variables that a player needs. Also, contains setters and getters for all attributes
+*  of a Player object.
+*/
+
 package edu.up.cs371.schmidtj.football;
 
 import java.io.Serializable;
@@ -7,113 +14,142 @@ import java.io.Serializable;
  */
 public class Player implements Serializable{
 
+    //Declaration of instance variables for each player object
     private String playersFullName;
     private int Goals;
     private int Assists;
     private String imageID;
 
+    //Constructor for Player object
     protected Player(String name, int GoalsStat, int AssistsStat)
     {
+        //Sets players name and stats to parameters
         this.playersFullName=name;
         this.Goals=GoalsStat;
         this.Assists=AssistsStat;
 
+        //Sets imageID to green_cran (default)
         this.setImageID("green_cran");
     }
 
+    //setImageID method: takes a string parameter and sets a Player's imageID to parameter
     public int setImageID(String imageName)
     {
+        //return 1 if success
         try
         {
             this.imageID= imageName;
             return 1;
         }
+        //return 0 if exception or something goes wrong
         finally
         {
             return 0;
         }
     }
 
+    //getImageID method: returns imageID of Player object
     public String getImageID()
     {
         return this.imageID;
     }
 
+    //setGoals method: sets a Player object's goals to the int parameter plus a Player's existing goals
     public int setGoals(int GoalsStat)
     {
+        //return 1 for success
         try
         {
             this.Goals+=GoalsStat;
             return 1;
         }
+        //return 0 if exception or something goes wrong
         finally
         {
             return 0;
         }
     }
 
+    //setGoals method: takes a string as a parameter then converts string into int and sets a Player's
+    //goals to be the sum of the converted int and a Player's existing goals.
     public int setGoals(String GoalsStat)
     {
+        //return 1 for success
         try
         {
             this.Goals+=Integer.parseInt( GoalsStat );
             return 1;
         }
+        //return 0 if exception or something goes wrong
         finally
         {
             return 0;
         }
     }
 
+    //getGoals method: returns the number of goals of a Player object
     public int getGoals()
     {
         return this.Goals;
     }
 
+    //setAssists method: sets a Player object's assists to the int parameter plus a Player's existing assists
     public int setAssists(int AssistsStat)
     {
+        //return 1 for success
         try
         {
             this.Assists+=AssistsStat;
             return 1;
         }
+        //return 0 if exception or something goes wrong
         finally
         {
             return 0;
         }
     }
 
+    //setAssists method: takes a string as a parameter then converts string into int and sets a Player's
+    //goals to be the sum of the converted int and a Player's existing assists.
     public int setAssists(String AssistsStat)
     {
+        //return 1 for success
         try
         {
             this.Assists+=Integer.parseInt( AssistsStat );
             return 1;
         }
+        //return 0 if exception or something goes wrong
         finally
         {
             return 0;
         }
     }
 
+    //getAssistsStat method: returns the number of assists of a Player object
     public int getAssistsStat()
     {
         return this.Assists;
     }
 
+    //setPlayersFullName method: takes a string as a parameter and sets a Player object's name to the
+    //string parameter
     public int setPlayersFullName(String Name)
     {
+        //return 1 for success
         try
         {
             this.playersFullName=Name;
             return 1;
         }
+        //return 0 if exception or something goes wrong
         finally
         {
             return 0;
         }
     }
 
+    //getPlayersFullName method: returns the name of a Player object
     public String getPlayersFullName()
     {
         return this.playersFullName;
--- app/src/main/java/edu/up/cs371/schmidtj/football/Team.java
+++ app/src/main/java/edu/up/cs371/schmidtj/football/Team.java
@@ -1,3 +1,11 @@
+/*
+* Team.java description:
+*  Class used for Team objects in Fantasy Football app. Constructor creates a Team object and initializes
+*  all variables that a team needs. Handles storage of Player objects in a certain Team object.
+*  Contains methods to edit a Team's variables and a Player object's stats. Also contains setters
+*  and getters for several variables.
+*/
+
 package edu.up.cs371.schmidtj.football;
 
 import java.io.Serializable;
@@ -9,6 +17,8 @@ import java.util.HashMap;
  */
 public class Team implements Serializable{
 
+    //Declaration of instance variables for each team object, arrayList of player names, and
+    //Hashmap of team roster
     private String teamsName;
     private int gamesWins;
     private int gamesPlayed;
@@ -17,33 +27,44 @@ public class Team implements Serializable{
     public ArrayList<String> playerList;
     private HashMap<String,Player> teamRoster;
 
+    //Team constructor: initializes instance variables for a Team object created, and creates ArrayList
+    //and Hashmap of players for each team. Also, creates a manager for each team.
     protected Team(String name, int games_Wins, int games_Lost)
     {
         this.teamsName=name;
 
+        //no negative number of wins possible
         if(games_Wins < 0)
             games_Wins = 0;
         this.gamesWins=games_Wins;
 
+        //no negative number of losses possible
         if(games_Lost < 0)
             games_Lost = 0;
         this.gamesLost=games_Lost;
 
+        //amount of games played for each team
         this.gamesPlayed = this.gamesLost+this.gamesWins;
 
+        //imageID for team
         this.setImageID("green_cran");
 
+        //Hashmap and ArrayList of players for each team
         playerList = new ArrayList<String>();
         teamRoster = new HashMap<String,Player>();
 
+        //Create team manager and add manager to team
         Player teamManager = new Player("TheBigFish",0,0);
         teamManager.setImageID("orange_fish");
 
         this.addAPlayer(teamManager);
     }
 
+    //addAPlayer method: adds a player object to the Hashmap of the team object that invoked this method
     public int addAPlayer(Player instPlayer)
     {
+        //put player object from parameter in Tean Hashmap and if their name is not in the arrayList
+        //add it to the arrayList. Return 1 for success
         try
         {
             teamRoster.put(instPlayer.getPlayersFullName(), instPlayer);
@@ -51,51 +72,62 @@ public class Team implements Serializable{
                 playerList.add(instPlayer.getPlayersFullName());
             return 1;
         }
+        //If there is an exception or something goes wrong return 0
         finally
         {
             return 0;
         }
     }
 
+    //getPlayer method: returns the Player object from a Team's hashmap using the player's name as the key
     public Player getPlayer(String playerName)
     {
         return teamRoster.get(playerName);
     }
 
+    //setImageID method: assign imageName parameter to be imageID of Team object that invoked this method
     public int setImageID(String imageName)
     {
+        //return 1 if success
         try
         {
             this.imageID= imageName;
             return 1;
         }
+        //return 0 if exception or something goes wrong
         finally
         {
             return 0;
         }
     }
 
+    //getImageID method: returns imageID of Team object that invoked this method
     public String getImageID()
     {
         return this.imageID;
     }
 
+    //updateWin method: increments a Team object's wins by 1 and calls updatePlayed to increments games
+    //played for this team object by 1.
     public void updateWin()
     {
         this.gamesWins++;
         this.updatePlayed();
     }
 
+    //getWin method: returns the wins of the Team object that invoked this method
     public int getWin()
     {
         return this.gamesWins;
     }
 
+    //updatePlayed method: increments the gamesPlayed of Team object that invoked this method by 1
     private void updatePlayed()
     {
         this.gamesPlayed++;
     }
 
+    //setWins method: sets a Team object's wins to the int parameter passed with method call
     public int setWins(int games_Wins)
     {
         try
@@ -109,84 +141,106 @@ public class Team implements Serializable{
         }
     }
 
+    //setWins method: takes a string parameter and converts it to an integer, then sets a Team object's
+    //wins to the newly converted integer
     public int setWins(String games_Wins)
     {
+        //return 1 for success
         try
         {
             this.gamesWins=Integer.parseInt(games_Wins);
             return 1;
         }
+        //return 0 for success
         finally
         {
             return 0;
         }
     }
 
+    //getPlayed method: return number of gamesPlayed for Team object that invoked method
     public int getPlayed()
     {
         return this.gamesPlayed;
     }
 
+    //updateLoses method: increments a Team object's losses by 1 and calls updatePlayed to increments games
+    //played for this team object by 1.
     public void updateLoses()
     {
         this.gamesLost++;
         this.updatePlayed();
     }
 
+    //getLoses method: returns the losses of the Team object that invoked this method
     public int getLoses()
     {
         return this.gamesLost;
     }
 
+    //setLoses method: sets a Team object's losses to the int parameter passed with method call
     public int setLoses(int games_lost)
     {
+        //return 1 for success
         try
         {
             this.gamesLost=games_lost;
             return 1;
         }
+        //return 0 if exception or something goes wrong
         finally
         {
             return 0;
         }
     }
 
+    //setLoses method: takes a string parameter and converts it to an integer, then sets a Team object's
+    //losses to the newly converted integer
     public int setLoses(String games_lost)
     {
+        //return 1 for success
         try
         {
             this.gamesLost=Integer.parseInt(games_lost);
             return 1;
         }
+        //return 0 if exception or something goes wrong
         finally
         {
             return 0;
         }
     }
 
+    //setTeamFullName method: takes a string parameter and sets a team objects name to the string parameter
     public int setTeamFullName(String Name)
     {
+        //return 1 for success
         try
         {
             this.teamsName=Name;
             return 1;
         }
+        //return 0 if exception or something goes wrong
         finally
         {
             return 0;
         }
     }
 
+    //getTeamFullName method: returns the name of a team
     public String getTeamFullName()
     {
         return this.teamsName;
     }
 
+    //updatePlayers method: edit the stats of a player object in a Team's hashmap
     public int updatePlayers(String playerName, int Assists, int Goals)
     {
+        //if the player is not in the hashmap return 0
         if( teamRoster.get(playerName) == null)
             return 0;
 
+        //set assists and goals. Return 1 for success.
         teamRoster.get(playerName).setAssists(Assists);
         teamRoster.get(playerName).setAssists(Goals);
 
